de02f228ebcda08ebc39de3d35b67ab06e3987b3
Haxrox <56497846+Haxrox@users.noreply.github.com>
2022-06-14 11:23:08 -0700

    initial commit

---
+++ app/src/main/java/com/haxtech/networkvideostream/MainActivity.java
@@ -0,0 +1,21 @@
+package com.haxtech.networkvideostream;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.haxtech.networkvideostream.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
